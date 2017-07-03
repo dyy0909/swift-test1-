874979dd160b6e5066f1a9c2177a975384b5c35d
yourname <dongyangyang@adinnet.cn>
2017-07-03 11:47:08 +0800

    first commit

---
+++ swifttest1day/swifttest1day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swifttest1day
+//
+//  Created by 董洋洋 on 2017/7/3.
+//  Copyright © 2017年 Adinnet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
